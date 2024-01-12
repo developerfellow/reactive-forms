8344e1ba13ffd7e8fc6886b3ad1dbbeb7ff64126
Varun Surekha <sureka.varun.dev@gmail.com>
2024-01-12 10:10:23 +0530

    Added reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import { FormGroup,FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reactive-forms';
+  signupForm:FormGroup;
+  constructor(){
+    this.signupForm = new FormGroup({
+      username:new FormControl('',Validators.required),
+      email:new FormControl('',Validators.required)
+    })
+  }
+  onSubmitHandler(){
+    console.log(this.signupForm);
+  }
 }
